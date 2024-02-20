06989c6334dcebbef9e5b4d812c5bbe6788553c3
Pranay  Nimsatkar <pranaynimsatkar@Pranays-MacBook-Air.local>
2024-02-20 22:12:09 +0530

    episode-1

---
+++ Day 1/App.js
@@ -0,0 +1,48 @@
+// * To create Siblings in React
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "This is h1"),
+//     React.createElement("h2", {}, "This is h2"),
+//   ])
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
+// * To create a nested structure in react
+
+//   const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//       "div",
+//       { id: "Child" },
+//       React.createElement("h1", {}, "Hi i am h1")
+//     )
+//   );
+
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//   root.render(parent);
+
+// * To create Hello World using React
+
+// const heading = React.createElement("h1" , {} , "Hello World from React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+//  * To create a hello world using javascript
+
+// const heading = document.createElement("h1");
+
+// heading.innerHTML = "Hello World!";
+
+// const root = document.getElementById("root");
+
+// root.appendChild(heading);
